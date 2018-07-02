5568cb02c11c55d115c2b1e2004eaa94c59b67d7
Waitire Colline <collinewait17@gmail.com>
2018-07-03 01:13:54 +0300

    Comment out tables test

--- api/tests/test_tablescreation.py
+++ api/tests/test_tablescreation.py
@@ -1,14 +1,14 @@
-"""
-This module creates tests table creation
-"""
+#"""
+#This module creates tests table creation
+#"""
 
-from unittest import TestCase
+#from unittest import TestCase
 #from unittest.mock import patch
 
-class TestTablesTestCase(TestCase):
-    """
-    Tests run for creation of tables
-    """
+#class TestTablesTestCase(TestCase):
+   # """
+   # Tests run for creation of tables
+    #"""
     #@patch("psycopg2.connect")
-    def test_user_table_created(self, mock_connect):
-        pass
+    #def test_user_table_created(self, mock_connect):
+        
