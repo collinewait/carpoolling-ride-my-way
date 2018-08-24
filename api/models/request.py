3c766d3ad617f5b2546e0cd538e86761af67b797
Colline Waitire <collinewaitire@Collines-MacBook-Pro.local>
2018-08-25 01:34:37 +0300

    Relocate checking of request existance to Request class

--- api/models/request.py
+++ api/models/request.py
@@ -33,4 +33,17 @@ class Request(object):
             "user_id": self.user_id,
             "ride_id": self.ride_id
         }
-        
+
+    def check_request_existance(self):
+        """
+        Checks the existance of a request made by a user.
+        I a request exists, it returns a failure message that the request already exists
+        otherwise, a success message is returned showing that a request does not exist.
+        """
+        check_sql = """SELECT "user_id", "ride_id" FROM "request"
+        WHERE "user_id" = %s AND "ride_id" = %s"""
+        request_data = (self.user_id, self.ride_id)
+        ride_request = DbTransaction.retrieve_one(check_sql, request_data)
+        if ride_request is None:
+            return {"status": "success", "message": "Request does not exists"}
+        return {"status": "failure", "message": "Request already exists"}
--- api/models/rides.py
+++ api/models/rides.py
@@ -142,28 +142,14 @@ class RidesHandler(object):
         if db_ride_id is None:
             return self.error_message.no_ride_available(ride_id)
 
-        check_request = self.check_request_existance(user_id, ride_id)
-        if check_request["status"] == "failure":
-            return jsonify({"message": check_request["message"]}), 400
         ride_request = Request(user_id, ride_id)
 
+        check_request = ride_request.check_request_existance()
+        if check_request["status"] == "failure":
+            return jsonify({"message": check_request["message"]}), 400
+        
         ride_request.save_request()
 
         return jsonify({"Status code": 201, 
                        "request": ride_request.return_request_information(),
                         "message": "request sent successfully"}), 201
-
-    @staticmethod
-    def check_request_existance(user_id, ride_id):
-        """
-        Checks the existance of a request made by a user.
-        I a request exists, it returns a failure message that the request already exists
-        otherwise, a success message is returned showing that a request does not exist.
-        """
-        check_sql = """SELECT "user_id", "ride_id" FROM "request"
-        WHERE "user_id" = %s AND "ride_id" = %s"""
-        request_data = (user_id, ride_id)
-        ride_request = DbTransaction.retrieve_one(check_sql, request_data)
-        if ride_request is None:
-            return {"status": "success", "message": "Request does not exists"}
-        return {"status": "failure", "message": "Request already exists"}
